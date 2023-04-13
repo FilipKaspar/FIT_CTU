da4742b15319aba1335bf569860a786e08075123
Filip Kašpar <kaspafil@fit.cvut.cz>
2023-04-13 14:26:44 +0200

    Updated repository

--- OSY/PT1/solution.cpp
+++ OSY/PT1/solution.cpp
@@ -1,4 +1,5 @@
 #ifndef __PROGTEST__
+
 #include <cstdio>
 #include <cstdlib>
 #include <cstring>
@@ -35,54 +36,64 @@
 #include <semaphore.h>
 #include "progtest_solver.h"
 #include "sample_tester.h"
+
 using namespace std;
 #endif /* __PROGTEST__ */
 
 //-------------------------------------------------------------------------------------------------------------------------------------------------------------
-class CCompanyHandler{
+class CCompanyHandler {
 public:
-    explicit CCompanyHandler(ACompany & id){
+    explicit CCompanyHandler(ACompany &id) {
         company_id = id;
+        to_send_empty = make_shared<condition_variable>();
     }
 
 //private:
+    shared_ptr<condition_variable> to_send_empty;
     ACompany company_id;
 
     thread wait_thread;
     thread solved_thread;
+    unsigned long int last_solved = 0;
 
-    queue<pair<unsigned long int,AProblemPack>> problems;
-    // Long int - indicates how many problems from pack has been loaded to solver if = problempack.size + 2. Then whole pack is finished
+    vector<pair<unsigned long int *, AProblemPack>> problems;
+    // Long int - indicates how many problems from pack has been loaded to solver if = problempack.size + 1. Then whole pack is finished
 };
 
-class COptimizer
-{
+class COptimizer {
 public:
-    static bool                        usingProgtestSolver                     ( void )
-    {
+    static bool usingProgtestSolver(void) {
         return true;
     }
-    static void                        checkAlgorithm                          ( AProblem                              problem )
-    {
+
+    static void checkAlgorithm(AProblem problem) {
         // dummy implementation if usingProgtestSolver() returns true
     }
-    void start (int threadCount);
-    void stop ();
-    void addCompany (ACompany company );
-    void waitThreads(CCompanyHandler & company);
-    void solveThreads(CCompanyHandler & company);
-    void workingThreads(AProgtestSolver & solver);
 
-private:
+    void start(int threadCount);
+
+    void stop();
+
+    void addCompany(ACompany company);
+
+    void waitThreads(CCompanyHandler &company);
+
+    void solveThreads(CCompanyHandler &company);
+
+    void workingThreads(AProgtestSolver &solver);
+
+//private:
     condition_variable to_solve_empty;
-    condition_variable to_send_empty;
     mutex wrk1;
+    AProgtestSolver solver;
+
+    int thread_count;
 
     vector<CCompanyHandler> companies;
 
     vector<thread> work_threads;
-    queue<CCompanyHandler*> all_problems;
-    vector<pair<unsigned long int, AProblemPack>*> problems_to_set;
+    deque<pair<unsigned long int, CCompanyHandler *>> all_problems;
+    vector<pair<unsigned long int *, CCompanyHandler *>> problems_to_set;
 };
 
 // TODO: COptimizer implementation goes here
@@ -92,133 +103,181 @@ void COptimizer::addCompany(ACompany company) {
 }
 
 void COptimizer::start(int threadCount) {
-    for(auto & company : companies){
+    thread_count = threadCount;
+
+    solver = createProgtestSolver();
+    for (int i = 0; i < threadCount; i++) {
+        work_threads.emplace_back(&COptimizer::workingThreads, this, ref(solver));
+    }
+
+    for (auto &company: companies) {
         company.wait_thread = thread(&COptimizer::waitThreads, this, ref(company));
         company.solved_thread = thread(&COptimizer::solveThreads, this, ref(company));
     }
-
-    auto solver = createProgtestSolver();
-    for(int i = 0; i < threadCount; i++){
-        work_threads.emplace_back(&COptimizer::workingThreads, this,ref(solver));
-    }
 }
 
-void COptimizer::waitThreads(CCompanyHandler & company) {
+void COptimizer::waitThreads(CCompanyHandler &company) {
     AProblemPack cur_problem_pack = company.company_id->waitForPack();
     unique_lock<mutex> lock(wrk1);
     lock.unlock();
-    while(cur_problem_pack != nullptr){
-        company.problems.push(make_pair(0, cur_problem_pack));
 
+    unsigned long int cnt = 0;
+    while (cur_problem_pack != nullptr) {
         lock.lock();
-        all_problems.push(&company);
+        company.problems.emplace_back(new unsigned long(0), cur_problem_pack);
+
+        all_problems.emplace_back(cnt, &company);
         lock.unlock();
 
         to_solve_empty.notify_one();
         cur_problem_pack = company.company_id->waitForPack();
+        cnt++;
     }
-
+    lock.lock();
+    company.problems.emplace_back(nullptr, nullptr);
+    lock.unlock();
+    company.to_send_empty->notify_one();
+    //cout << "WaitThread skončil" << endl;
 }
 
-void COptimizer::solveThreads(CCompanyHandler & company) {
-    while(true){
+void COptimizer::solveThreads(CCompanyHandler &company) {
+    while (true) {
         unique_lock<mutex> lock(wrk1);
-        to_solve_empty.wait(lock, [&company]() {
-            if(company.problems.empty()) return false;
-            if(!company.problems.front().second) return false;
-            return company.problems.front().first == company.problems.front().second->m_Problems.size() + 2; });
+        company.to_send_empty->wait(lock, [&company]() {
+            if (company.problems.empty() || company.last_solved >= company.problems.size()) return false;
+            if (!company.problems[company.last_solved].second) return true;
+            return *company.problems[company.last_solved].first ==
+                   company.problems[company.last_solved].second->m_Problems.size() + 1;
+        });
 
+        //cout << "SolveThread vybuzněn" << endl;
 
-        auto problems = &company.problems.front();
-        if(!problems->second) break;
-        while(problems->first == problems->second->m_Problems.size() + 2){
-            company.company_id->solvedPack(problems->second);
-            company.problems.pop();
-            problems = &company.problems.front();
+        if(company.last_solved >= company.problems.size()) continue;
+        auto problem = company.problems[company.last_solved];
+        //lock.unlock();
+        if (!problem.first || !problem.second) break;
+
+        while (*problem.first == problem.second->m_Problems.size() + 1) {
+            company.company_id->solvedPack(problem.second);
+            //lock.lock();
+
+            delete company.problems[company.last_solved].first;
+            company.problems[company.last_solved].second = nullptr;
+
+            company.last_solved++;
+            if(company.last_solved >= company.problems.size()) break;
+            problem = company.problems[company.last_solved];
+            //lock.unlock();
+
+            if (!problem.first || !problem.second) return;
         }
     }
 }
 
 
 void COptimizer::workingThreads(AProgtestSolver & solver) {
-    while(true){
+    while (true) {
         unique_lock<mutex> lock(wrk1);
         to_solve_empty.wait(lock, [this]() { return !all_problems.empty(); });
 
+        //cout << "WorkingThread začátek" << endl;
+
         auto cur_company = all_problems.front();
-        all_problems.pop();
-        if(!cur_company) break;
-        auto cur_problem = &cur_company->problems.front();
-        if(!cur_problem->second) continue;
+        all_problems.pop_front();
+        if (!cur_company.second) break;
+        auto cur_problem = cur_company.second->problems[cur_company.first];
         lock.unlock();
 
-        for(auto problem = cur_problem->second->m_Problems.begin() + int(cur_problem->first);
-                 problem != cur_problem->second->m_Problems.end(); problem++) {
-            if (solver->hasFreeCapacity()) {
-                lock.lock();
-                solver->addProblem(*problem); // Maybe check if returned 0, but should be covered by hasFreeCapacity
+        for (auto problem = cur_problem.second->m_Problems.begin() + int(*cur_problem.first);
+             problem != cur_problem.second->m_Problems.end(); problem++) {
+            lock.lock();
+            if (!solver->addProblem(*problem)) {
                 lock.unlock();
-            } else {
                 break;
             }
-            cur_problem->first++;
+            lock.unlock();
+            (*(cur_problem.first))++;
         }
 
-        if(cur_problem->second->m_Problems.size() == cur_problem->first - 1) {
-            lock.lock();
-            problems_to_set.push_back(cur_problem);
-            lock.unlock();
+        lock.lock();
+        if (cur_problem.second->m_Problems.size() == *cur_problem.first) {
+            problems_to_set.emplace_back(cur_problem.first, cur_company.second);
+        } else {
+            all_problems.push_front(cur_company);
+            to_solve_empty.notify_one();
         }
         //If we used all the problems, set to counter to the size of the problem array
 
-        lock.lock();
         // Checking here one more time, in case the last problem has been loaded, therefore the for loop is gonna end and it would have to
         // wait for another thread call to call the solve method
-        if(!solver->hasFreeCapacity()){
-            lock.unlock();
+        if (!solver->hasFreeCapacity()) { // Segfault here??
             auto copy_solver = solver;
-            lock.lock();
             solver = createProgtestSolver();
-            lock.unlock();
-            copy_solver->solve();
-            lock.lock();
-            for(auto & problem : problems_to_set){
-                problem->first = problem->second->m_Problems.size() + 2;
-            }
+            auto copy = problems_to_set;
             problems_to_set.clear();
             lock.unlock();
-            to_send_empty.notify_all();
+            copy_solver->solve();
+            //cout << "Solver activated" << endl;
+            lock.lock();
+            for (auto &problem: copy) {
+                (*problem.first)++;
+                problem.second->to_send_empty->notify_one();
+            }
         }
+        //cout << "WorkingThread konec" << endl;
     }
 }
 
 void COptimizer::stop() {
-    for(auto & company : companies){
-        company.problems.push(make_pair(0, nullptr));
+    unique_lock<mutex> lock(wrk1);
+    lock.unlock();
+    for (auto &company: companies) {
         company.wait_thread.join();
     }
-    unique_lock<mutex> lock(wrk1);
-    all_problems.push(nullptr);
+    lock.lock();
+
+    for (int i = 0; i < thread_count; i++) all_problems.emplace_back(420, nullptr);
     to_solve_empty.notify_all();
     lock.unlock();
-    for(auto & work_thread : work_threads){
+
+    for (auto &work_thread: work_threads) {
         work_thread.join();
     }
-    for(auto & company : companies){
+
+    lock.lock();
+    if (!problems_to_set.empty()) {
+        solver->solve();
+        for (auto &problem: problems_to_set) {
+            (*problem.first)++;
+            problem.second->to_send_empty->notify_one();
+        }
+        problems_to_set.clear();
+    }
+    lock.unlock();
+
+    for (auto &company: companies) {
         company.solved_thread.join();
     }
 }
 //-------------------------------------------------------------------------------------------------------------------------------------------------------------
 #ifndef __PROGTEST__
-int                                    main                                    ( void )
-{
-    COptimizer optimizer;
-    ACompanyTest  company = std::make_shared<CCompanyTest> ();
-    optimizer . addCompany ( company );
-    optimizer . start ( 4 );
-    optimizer . stop  ();
-    if ( ! company -> allProcessed () )
-        throw std::logic_error ( "(some) problems were not correctly processsed" );
+
+int main(void) {
+    for(int i = 0; i < 1; i++) {
+        COptimizer optimizer;
+        ACompanyTest company = std::make_shared<CCompanyTest>();
+        ACompanyTest companyb = std::make_shared<CCompanyTest>();
+        optimizer.addCompany(company);
+        optimizer.addCompany(companyb);
+        optimizer.start(50);
+        optimizer.stop();
+        if (!company->allProcessed())
+            throw std::logic_error("(some) problems were not correctly processsed");
+        if (!companyb->allProcessed())
+            throw std::logic_error("(some) problems were not correctly processsed");
+        //cout << i << endl;
+    }
     return 0;
 }
+
 #endif /* __PROGTEST__ */
--- PA2/Finals/Theoretical/cmake-build-debug-wsl/CMakeFiles/Finals.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for Finals.
-# This may be replaced when dependencies are built.
